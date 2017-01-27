f956e7761b0f3f3a1c9b4e5dde565da0ef6c11e7
Okko Kauhanen <okko.kauhanen@mediconsult.fi>
2017-01-27 14:27:42 +0200

    MT-156 Added UI for PHR

--- src/main/java/fi/oda/phr/config/WebConfig.java
+++ src/main/java/fi/oda/phr/config/WebConfig.java
@@ -2,11 +2,13 @@ package fi.oda.phr.config;
 
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import fi.oda.phr.JpaServer;
 
 @Configuration
+@ComponentScan("ca.uhn.fhir.to")
 public class WebConfig {
 
   private final JpaServer jpaFhirServer;
