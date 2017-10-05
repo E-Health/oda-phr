ba323ee86fca7dae4131e50259c1ab1d7348af15
Okko Kauhanen <okko.kauhanen@mediconsult.fi>
2017-10-05 16:32:54 +0300

    KT-1283 first implementation and tests

---
+++ src/main/java/fi/oda/phr/server/interceptor/OdaInstanceIdConcatenatingInterceptor.java
@@ -0,0 +1,120 @@
+package fi.oda.phr.server.interceptor;
+
+
+import java.util.*;
+import java.util.stream.*;
+
+import javax.servlet.http.*;
+
+import org.apache.commons.lang3.StringUtils;
+import org.hl7.fhir.dstu3.model.*;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+
+import ca.uhn.fhir.rest.api.RestOperationTypeEnum;
+import ca.uhn.fhir.rest.method.RequestDetails;
+import ca.uhn.fhir.rest.server.exceptions.AuthenticationException;
+import ca.uhn.fhir.rest.server.interceptor.InterceptorAdapter;
+import ca.uhn.fhir.rest.server.servlet.ServletRequestDetails;
+import fi.oda.common.fhir.utils.OdaFhirConstants;
+
+/**
+ * Server interceptor for appending odaInstanceId to national ids found in resources and query parameters.
+ * 
+ * National ids will be persisted in the following format: xxxxxx-xxxxINSTANCE-ID, where INSTANCE-ID
+ * is the odaInstanceId obtained from ODA-INSTANCE-ID HTTP header.
+ * 
+ * The suffix is removed from identifiers in returned resources and bundles
+ */
+public class OdaInstanceIdConcatenatingInterceptor extends InterceptorAdapter {
+
+    public static final String IDENTIFIER_PARAMETER = "identifier";
+
+    //Query parameters are processed for these types
+    private Set<String> types = Collections.unmodifiableSet(
+            Stream.of("Person")
+                    .collect(Collectors.toSet()));
+
+    /**
+     * Concatenates odaInstanceId to nationalid query parameters
+     */
+    @Override
+    public boolean incomingRequestPostProcessed(RequestDetails theRequestDetails, HttpServletRequest theRequest,
+            HttpServletResponse theResponse) throws AuthenticationException {
+        String resourceType = theRequestDetails.getResourceName();
+        if (StringUtils.isNotBlank(resourceType) && types.contains(resourceType)) {
+            ServletRequestDetails requestDetails = (ServletRequestDetails) theRequestDetails;
+            String headerValue = requestDetails.getServletRequest().getHeader(OdaFhirConstants.ODA_INSTANCE_ID_HEADER);
+            if (StringUtils.isNotBlank(headerValue)) {
+                addToQueryParameters(requestDetails, headerValue);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Concatenates odaInstanceId to Person resource identifier (urn:oid:1.2.246.21)
+     */
+    @Override
+    public void incomingRequestPreHandled(RestOperationTypeEnum theOperation, ActionRequestDetails theProcessedRequest) {
+        ServletRequestDetails requestDetails = (ServletRequestDetails) theProcessedRequest.getRequestDetails();
+        String headerValue = requestDetails.getServletRequest().getHeader(OdaFhirConstants.ODA_INSTANCE_ID_HEADER);
+        if (StringUtils.isNotBlank(headerValue)) {
+            addToResource(theProcessedRequest.getResource(), headerValue);
+        }
+
+    }
+
+    /**
+     * Removes odaInstanceId from returned Person resources (also within Bundles)
+     */
+    @Override
+    public boolean outgoingResponse(RequestDetails theRequestDetails, IBaseResource resource) {
+        if (resource instanceof Resource) {
+            removeFromResource((Resource) resource);
+        }
+        return true;
+    }
+
+    private void removeFromResource(Resource resource) {
+        if (resource instanceof Bundle) {
+            Bundle bundle = (Bundle) resource;
+            if (bundle.getEntry() != null) {
+                bundle.getEntry().stream().map(e -> e.getResource()).filter(Objects::nonNull).forEach(r -> removeFromResource(r));
+            }
+        }
+        else if (resource instanceof Person) {
+            List<Identifier> identifiers = ((Person) resource).getIdentifier();
+            identifiers.stream().filter(i -> OdaFhirConstants.NATIONAL_ID_SYSTEM.equals(i.getSystem())).findFirst().ifPresent(
+                    j -> j.setValue(j.getValue().substring(0, 11)));
+        }
+    }
+
+    private void addToResource(IBaseResource resource, String odaInstanceId) {
+        if (resource instanceof Person) {
+            List<Identifier> identifiers = ((Person) resource).getIdentifier();
+            identifiers.stream().filter(i -> OdaFhirConstants.NATIONAL_ID_SYSTEM.equals(i.getSystem())).findFirst().ifPresent(
+                    j -> j.setValue(j.getValue() + odaInstanceId));
+        }
+
+    }
+
+    private void addToQueryParameters(RequestDetails details, String odaInstanceId) {
+        Map<String, String[]> parameters = details.getParameters();
+        if (parameters.containsKey(IDENTIFIER_PARAMETER)) {
+            parameters.put(IDENTIFIER_PARAMETER,
+                    Arrays.stream(parameters.get(IDENTIFIER_PARAMETER))
+                            .map(m -> processQueryIdentifier(m, odaInstanceId))
+                            .toArray(String[]::new));
+        }
+    }
+
+    private String processQueryIdentifier(String identifier, String odaInstanceId) {
+        String result = identifier;
+        String[] splitIdentifier = identifier.split("\\|");
+        if (splitIdentifier.length == 2 && splitIdentifier[0].equals(OdaFhirConstants.NATIONAL_ID_SYSTEM)) {
+            return result + odaInstanceId;
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/fi/oda/phr/server/interceptor/OdaInstanceIdConcatenatingInterceptorTest.java
@@ -0,0 +1,117 @@
+package fi.oda.phr.server.interceptor;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+
+import java.util.*;
+
+import javax.servlet.http.*;
+
+import org.hl7.fhir.dstu3.model.*;
+import org.junit.*;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.api.RestOperationTypeEnum;
+import ca.uhn.fhir.rest.server.RestfulServer;
+import ca.uhn.fhir.rest.server.interceptor.IServerInterceptor.ActionRequestDetails;
+import ca.uhn.fhir.rest.server.servlet.ServletRequestDetails;
+import fi.oda.common.fhir.utils.OdaFhirConstants;
+
+public class OdaInstanceIdConcatenatingInterceptorTest {
+    private OdaInstanceIdConcatenatingInterceptor interceptor;
+
+    private ServletRequestDetails requestDetails;
+
+    private HttpServletRequest theRequest;
+
+    private HttpServletResponse theResponse;
+
+    private static final String resourceName = "Person";
+
+    private static final String odaInstanceId = "testinstance";
+
+    private String nationalId = "010101-0101";
+
+    private String nationalIdQueryParam = OdaFhirConstants.NATIONAL_ID_SYSTEM + "|" + nationalId;
+
+    private String concatenatedNationalId = nationalId + odaInstanceId;
+
+    private String concatenatedQueryParam = OdaFhirConstants.NATIONAL_ID_SYSTEM + "|" + concatenatedNationalId;
+
+    private Bundle bundle;
+    private Person personResource;
+    private Map<String, String[]> queryParams;
+
+    private RestfulServer mockServer;
+    private ActionRequestDetails actionRequestDetails;
+
+    @Before
+    public void setUp() {
+        queryParams = new HashMap<String, String[]>();
+        queryParams.put(OdaInstanceIdConcatenatingInterceptor.IDENTIFIER_PARAMETER, new String[]
+            { nationalIdQueryParam });
+        personResource = new Person();
+        personResource.addIdentifier().setSystem(OdaFhirConstants.NATIONAL_ID_SYSTEM).setValue(nationalId);
+        interceptor = new OdaInstanceIdConcatenatingInterceptor();
+        requestDetails = mock(ServletRequestDetails.class);
+        theRequest = mock(HttpServletRequest.class);
+        theResponse = mock(HttpServletResponse.class);
+        mockServer = mock(RestfulServer.class);
+        
+        when(requestDetails.getResourceName()).thenReturn(resourceName);
+        when(requestDetails.getServletRequest()).thenReturn(theRequest);
+        when(requestDetails.getParameters()).thenReturn(queryParams);
+        when(theRequest.getHeader(OdaFhirConstants.ODA_INSTANCE_ID_HEADER)).thenReturn(odaInstanceId);
+        when(mockServer.getFhirContext()).thenReturn(FhirContext.forDstu3());
+        when(requestDetails.getServer()).thenReturn(mockServer);
+        
+        actionRequestDetails = new ActionRequestDetails(requestDetails);
+        actionRequestDetails.setResource(personResource);
+        bundle = new Bundle();
+        bundle.addEntry().setResource(personResource);
+    }
+
+
+    @Test
+    public void testConcatenateToQueryParam() {
+        interceptor.incomingRequestPostProcessed(requestDetails, theRequest, theResponse);
+        String producedQueryParam = queryParams.get(OdaInstanceIdConcatenatingInterceptor.IDENTIFIER_PARAMETER)[0];
+        assertThat(producedQueryParam, is(concatenatedQueryParam));
+    }
+
+    @Test
+    public void testConcatenateToPersonIdentifier() {
+        interceptor.incomingRequestPreHandled(RestOperationTypeEnum.CREATE, actionRequestDetails);
+        Identifier producedPersonIdentifier = personResource.getIdentifier().get(0);
+        assertThat(producedPersonIdentifier.getSystem(), is(OdaFhirConstants.NATIONAL_ID_SYSTEM));
+        assertThat(producedPersonIdentifier.getValue(), is(concatenatedNationalId));
+    }
+
+    @Test
+    public void testConcatenateToBundlePersonIdentifier() {
+        actionRequestDetails.setResource(bundle);
+        interceptor.incomingRequestPreHandled(RestOperationTypeEnum.CREATE, actionRequestDetails);
+        Identifier producedPersonIdentifier = personResource.getIdentifier().get(0);
+        assertThat(producedPersonIdentifier.getSystem(), is(OdaFhirConstants.NATIONAL_ID_SYSTEM));
+        assertThat(producedPersonIdentifier.getValue(), is(concatenatedNationalId));
+    }
+
+    @Test
+    public void testRemoveBundlePersonIdentifierSuffix() {
+        personResource.getIdentifier().get(0).setValue(concatenatedNationalId);
+        interceptor.outgoingResponse(requestDetails, bundle);
+        Identifier producedPersonIdentifier = personResource.getIdentifier().get(0);
+        assertThat(producedPersonIdentifier.getSystem(), is(OdaFhirConstants.NATIONAL_ID_SYSTEM));
+        assertThat(producedPersonIdentifier.getValue(), is(nationalId));
+    }
+
+    @Test
+    public void testRemovePersonIdentifierSuffix() {
+        personResource.getIdentifier().get(0).setValue(concatenatedNationalId);
+        interceptor.outgoingResponse(requestDetails, personResource);
+        Identifier producedPersonIdentifier = personResource.getIdentifier().get(0);
+        assertThat(producedPersonIdentifier.getSystem(), is(OdaFhirConstants.NATIONAL_ID_SYSTEM));
+        assertThat(producedPersonIdentifier.getValue(), is(nationalId));
+    }
+}
